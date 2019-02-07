0845ec406be0b5b0287087fb5a7c68fad111bb1c
demirtaserdem <demirtaserdem@gmail.com>
2019-02-07 15:19:55 +0300

    tab to space

--- yta_c_prj.py
+++ yta_c_prj.py
@@ -31,78 +31,78 @@ translate_url = base_url + key+base_lang + lang + base_text + text
 #####-----#####
 
 def clean_Last_Searched():
-	"""last_searchs.txt dosyasının temizlenmesinde ve oluşturulmasını
-	sağlar
-	"""
-	open("last_searchs.txt","w",encoding = "utf-8").close()
-	
+    """last_searchs.txt dosyasının temizlenmesinde ve oluşturulmasını
+    sağlar
+    """
+    open("last_searchs.txt","w",encoding = "utf-8").close()
+    
 def selections_list():
-	"""Seçim Listesidir
-	"""
-	print(
-	"""
-	*************************
-	Çeviri Uygulaması
+    """Seçim Listesidir
+    """
+    print(
+    """
+    *************************
+    Çeviri Uygulaması
 
-	1. Çeviri Yap. 
+    1. Çeviri Yap. 
 
-	2. Son Arananları göster.
+    2. Son Arananları göster.
 
-	3. Son Arananları temizle.
+    3. Son Arananları temizle.
 
-	Çıkmak İçin 'q' ya basın
-	*************************
-	
-	"""
-	)  
+    Çıkmak İçin 'q' ya basın
+    *************************
+    
+    """
+    )  
 
 def translateFunc(translate_url):
-	"""Oluşturulan url'ye istek gönderir, alır, json objesine çevirir
-	json objesinin içinden ilgili kısmı dataya yazar. Çevriyi Str 
-	olarak döndürür.
-	"""
-	data_get = requests.get(translate_url)
-	data_json = data_get.json()
-	data = data_json["text"][0]
-	return data
+    """Oluşturulan url'ye istek gönderir, alır, json objesine çevirir
+    json objesinin içinden ilgili kısmı dataya yazar. Çevriyi Str 
+    olarak döndürür.
+    """
+    data_get = requests.get(translate_url)
+    data_json = data_get.json()
+    data = data_json["text"][0]
+    return data
 
 
 def writeLastSearch(text,translated_word):
-	"""Çevrilen ve çeviri kelimeyi str olarak alır,
-	last_searchs.txt dosyasına yazar konsola yazabiliyorsa yazar, 
-	utf-8 hatasından - çince vs hata olursa hata mesajı verir.
-	"""
-	with open("last_searchs.txt","a",encoding = "utf-8") as file:
-		file.write(text +" --->>> "+ translated_word + "\n")
-	try:
-		print(text + " --->>> " + translated_word + "\n")
-	except:
-		print("""Konsola yazdırırken hata oluştu... txt dosyasını 
-			kontrol edebilirsiniz. \n""")
+    """Çevrilen ve çeviri kelimeyi str olarak alır,
+    last_searchs.txt dosyasına yazar konsola yazabiliyorsa yazar, 
+    utf-8 hatasından - çince vs hata olursa hata mesajı verir.
+    """
+    with open("last_searchs.txt","a",encoding = "utf-8") as file:
+        file.write(text +" --->>> "+ translated_word + "\n")
+    try:
+        print(text + " --->>> " + translated_word + "\n")
+    except:
+        print("""Konsola yazdırırken hata oluştu... txt dosyasını 
+            kontrol edebilirsiniz. \n""")
 
 def printLastSearch():
-	"""Son Arananların listesini
-	last_searchs.txt den alır. yazdırır
-	"""
-	with open("last_searchs.txt","r",encoding = "utf-8") as file:
-		print("*"*25)
-		print(file.read())
-		print("*"*25)
+    """Son Arananların listesini
+    last_searchs.txt den alır. yazdırır
+    """
+    with open("last_searchs.txt","r",encoding = "utf-8") as file:
+        print("*"*25)
+        print(file.read())
+        print("*"*25)
 
 def clean_sure_question():
-	"""Son Arananlar silinsin mi evet hayır seçeneği istiyor.
-	girdiyi küçültüp temizleyip kontrol ediyor true veya false 
-	dönüyor
-	"""
-	while True:
-		intext = "Son arananlar silinecek emin misiniz? [Y/n]: "
-		inputtemp = input(intext).lower().strip()
-		if inputtemp == "y" or inputtemp == "yes":
-			return True
-		elif inputtemp == "n" or inputtemp == "no":
-			return False
-		else:
-			print("Geçerli bir seçenek giriniz: ")
+    """Son Arananlar silinsin mi evet hayır seçeneği istiyor.
+    girdiyi küçültüp temizleyip kontrol ediyor true veya false 
+    dönüyor
+    """
+    while True:
+        intext = "Son arananlar silinecek emin misiniz? [Y/n]: "
+        inputtemp = input(intext).lower().strip()
+        if inputtemp == "y" or inputtemp == "yes":
+            return True
+        elif inputtemp == "n" or inputtemp == "no":
+            return False
+        else:
+            print("Geçerli bir seçenek giriniz: ")
 
 
 
@@ -114,44 +114,44 @@ clean_Last_Searched()
 
 while True:
 #Ana Döngü Seçeneklerden oluşuyor
-	selection = input("Seçeneği Giriniz: ")
-		
-	if selection == "1":
-		print("Çıkmak için 'q' tuşuna basınız. \n")
-		while True:
-		#q tuşuna basıncaya kadar sürekli kelime istiyor.
-			#kelimeyi aldık küçülttük
-			word = str(input("Kelimeyi Giriniz: ")).lower()
-			try:
-				#kelime q ise döngüden çıkıp listeyi tekrar yazdırdık.
-				if word == "q":
-					selections_list()
-					break
-				else:
-					#kelime tam geldiyse urlyi oluşturduk
-					text = word
-					translate_url = (base_url + key + base_lang + lang
-						+ base_text + text)
-					#çeviriyi yaptık
-					translated_word = translateFunc(translate_url)
-					#Yazdırma kısmını çalıştırdık.
-					writeLastSearch(word,translated_word)
-			except:
-				print("Hatalı kelime girişi - tekrar giriniz... \n")
+    selection = input("Seçeneği Giriniz: ")
+        
+    if selection == "1":
+        print("Çıkmak için 'q' tuşuna basınız. \n")
+        while True:
+        #q tuşuna basıncaya kadar sürekli kelime istiyor.
+            #kelimeyi aldık küçülttük
+            word = str(input("Kelimeyi Giriniz: ")).lower()
+            try:
+                #kelime q ise döngüden çıkıp listeyi tekrar yazdırdık.
+                if word == "q":
+                    selections_list()
+                    break
+                else:
+                    #kelime tam geldiyse urlyi oluşturduk
+                    text = word
+                    translate_url = (base_url + key + base_lang + lang
+                        + base_text + text)
+                    #çeviriyi yaptık
+                    translated_word = translateFunc(translate_url)
+                    #Yazdırma kısmını çalıştırdık.
+                    writeLastSearch(word,translated_word)
+            except:
+                print("Hatalı kelime girişi - tekrar giriniz... \n")
 
-	elif selection == "2":
-			if os.stat("last_searchs.txt").st_size == 0:
-				print("Son Aranan Kemlime Bulunmamaktadır. \n")
-			else: 
-				printLastSearch()    
+    elif selection == "2":
+            if os.stat("last_searchs.txt").st_size == 0:
+                print("Son Aranan Kemlime Bulunmamaktadır. \n")
+            else: 
+                printLastSearch()    
 
-	elif selection == "3":
-		if clean_sure_question():
-			clean_Last_Searched()
-			print("Son aramalar temizlendi.\n")
-		else:
-			print("Son aramalar temizlenemedi!!!.\n")
-		
-	elif selection == "q":
-		break
+    elif selection == "3":
+        if clean_sure_question():
+            clean_Last_Searched()
+            print("Son aramalar temizlendi.\n")
+        else:
+            print("Son aramalar temizlenemedi!!!.\n")
+        
+    elif selection == "q":
+        break
 
